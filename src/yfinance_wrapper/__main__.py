a3bc956bcbd9f50a630204cac7cb680fe875e20f
lward27 <lward@ippon.fr>
2023-11-15 16:21:58 -0500

    updated some things

--- src/yfinance_wrapper/__main__.py
+++ src/yfinance_wrapper/__main__.py
@@ -1,7 +1,11 @@
 import uvicorn
 from yfinance_wrapper.api import app
+import os
 
 def main():
+    print("YFinance Wrapper Starting Up!")
+    TEST_VARIABLE = os.getenv('TEST_VARIABLE')
+    print(TEST_VARIABLE)
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
