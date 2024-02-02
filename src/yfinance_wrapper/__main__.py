8f89863aa4b997caf63dda9dc231dece154f81ed
lward27 <lward@ippon.fr>
2024-02-02 10:26:57 -0500

    version 5 of yfinance wrapper, for ECS fargate

--- src/yfinance_wrapper/__main__.py
+++ src/yfinance_wrapper/__main__.py
@@ -1,12 +1,14 @@
 import uvicorn
 from yfinance_wrapper.api import app
 import os
+from prometheus_fastapi_instrumentator import Instrumentator
 
 def main():
     print("YFinance Wrapper Starting Up!")
     TEST_VARIABLE = os.getenv('TEST_VARIABLE')
     print(TEST_VARIABLE)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    Instrumentator().instrument(app, metric_subsystem='yfinance_wrapper').expose(app)
+    uvicorn.run(app, host="0.0.0.0", port=80)
 
 if __name__ == "__main__":
     main()
